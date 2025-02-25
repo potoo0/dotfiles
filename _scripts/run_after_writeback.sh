717ab0cd747ca2199f4aa9c6644d643c700eb228
potoo0 <1415615232@qq.com>
2025-02-25 23:40:13 +0800

    Initial commit

---
+++ _scripts/run_after_writeback.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+# cp -rp ~/.cache/dotfiles/etc /etc
---
+++ _scripts/run_before_readfrom.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir -p ~/.cache/dotfiles/etc
+
+cp -rp /etc/tmux.conf ~/.cache/dotfiles/etc
+cp -rp /etc/lf        ~/.cache/dotfiles/etc
