16b6b55cd4291d444a9f4f249d94fbd531e5c518
potoo0 <1415615232@qq.com>
2025-02-26 22:39:44 +0800

    update zshrc

--- _scripts/run_after_writeback.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-# cp -rp ~/.cache/dotfiles/etc /etc
--- _scripts/run_before_readfrom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p ~/.cache/dotfiles/etc
-
-cp -rp /etc/tmux.conf ~/.cache/dotfiles/etc
-cp -rp /etc/lf        ~/.cache/dotfiles/etc
